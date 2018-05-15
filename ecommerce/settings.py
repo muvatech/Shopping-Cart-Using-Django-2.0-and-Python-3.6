94bd9e87a3cd6881adc168593d49077a0cdb7cca
henrymbuguak <henrymbuguak@gmail.com>
2018-05-15 08:47:28 +0300

    Cart class to manage our shopping cart

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +123,5 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'products/')
+
+CART_SESSION_ID = 'cart'
