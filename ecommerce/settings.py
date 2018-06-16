e8c6a38095ece5375c24eca685a8c4170933afdf
henrymbuguak <henrymbuguak@gmail.com>
2018-06-16 12:10:23 +0300

    Orders added to admin site

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
+    'orders.apps.OrdersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
