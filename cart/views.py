a74fe42a447fe8552bb3925436166e5dc124b92b
Antonio Piacentini <piretto@hotmail.it>
2018-09-05 13:21:52 +0200

    Update views.py

--- cart/views.py
+++ cart/views.py
@@ -7,8 +7,8 @@ from .forms import CartAddProductForm
 
 @require_POST
 def cart_add(request, product_id):
-    cart = Cart(request)
-    product = get_object_or_404(Product, id=product_id)
+    cart = Cart(request)  # create a new cart object passing it the request object 
+    product = get_object_or_404(Product, id=product_id) 
     form = CartAddProductForm(request.POST)
     if form.is_valid():
         cd = form.cleaned_data
